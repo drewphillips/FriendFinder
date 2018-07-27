6ac015c8413bd0bdab510bff4826a37b3d6449ee
drwphillips <drew@superheroprinting.com>
2018-07-27 10:41:38 -0700

    working NPM installs and server codde

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
-
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// require("./app/routing/apiRoutes.js")(app);
+// require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
 
