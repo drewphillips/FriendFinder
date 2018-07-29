8eb3601b8a94bc9e543e53a7f0ce85e67233914a
drewphillips <drew@superheroprinting.com>
2018-07-29 14:49:28 -0700

    survey and matching logic

--- app/apiRoutes.js
+++ app/apiRoutes.js
@@ -2,5 +2,28 @@ module.exports = function (app) {
     app.post("/api/frineds", function(req, res){
         console.log(req.body)
         res.json(req.body)
+
+        var userData = req.body;
+        console.log(userData);
+
+        console.log(friends);
+        friends.push(userData);
     })
 }
+
+var bestmatch;
+        var bestscore = 999;
+
+        for (var i = 0; i < friends.length; i++) {
+            var score = 0;
+            for (var j = 0; j < 10; j++) {
+                var partialsum = Math.abs(friends[i].scores[j] - userData.scores[j]);
+                score += partialsum;
+            }
+            if (score < bestscore) {
+                bestscore = score;
+                bestmatch = i;
+            }
+        }
+
+        res.json(friends[bestmatch]);
--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./app/apiRoutes.js")(app);
 require("./app/htmlRoutes.js")(app);
