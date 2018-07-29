bb8ee2d63fbda7bb173b9377127bf61fc0237180
drewphillips <drew@superheroprinting.com>
2018-07-29 15:57:15 -0700

    fuck line 244

--- app/apiRoutes.js
+++ app/apiRoutes.js
@@ -1,33 +1,24 @@
 var friends = require("../app/data/friends");
 
 module.exports = function (app) {
-    app.post("/api/friedns", function(req, res){
-        console.log(req.body)
-        // res.json(req.body)
-
+    app.post("/api/friedns", function (req, res) {
         var userData = req.body;
         console.log(userData);
 
-        console.log(friends);
-        friends.push(userData);
-    });
+    var bestmatch;
+    var bestscore = 999;
 
-
-var bestmatch;
-        var bestscore = 999;
-
-        for (var i = 0; i < friends.length; i++) {
-            var score = 0;
-            for (var j = 0; j < 10; j++) {
-                var partialsum = Math.abs(friends[i].scores[j] - friends.scores);
-                score += partialsum;
-            }
-            if (score < bestscore) {
-                bestscore = score;
-                bestmatch = i;
-            }
+    for (var i = 0; i < friends.length; i++) {
+        var score = 0;
+        for (var j = 0; j < 10; j++) {
+            var partialsum = Math.abs(friends[i].scores[j] - friends.scores);
+            score += partialsum;
         }
-
+        if (score < bestscore) {
+            bestscore = score;
+            bestmatch = i;
+        }
     }
-     res.json(friends[bestmatch]);
-    
+    res.json(friends[bestmatch]);
+})
+}
